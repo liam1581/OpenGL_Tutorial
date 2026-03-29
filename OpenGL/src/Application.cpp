1b3e7ae0bb4e91d656bccf2b5dda8e72f3f3c5f2
liam1581 <liamschmallowsky123@gmail.com>
2026-03-29 13:12:06 +0200

    finished textures, blending and maths

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -12,8 +12,13 @@
 #include "VertexArray.h"
 #include "VertexBuffer.h"
 #include "VertexBufferLayout.h"
+#include "Texture.h"
+
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
 
 using namespace std;
+using namespace glm;
 
 int main(void) {
 	GLFWwindow* window;
@@ -41,11 +46,10 @@ int main(void) {
 	cout << glGetString(GL_VERSION) << endl;
 	{
 		float positions[] = {
-			-0.5f, -0.5f,
-			 0.5f, -0.5f,
-			 0.5f,  0.5f,
-
-			-0.5f,  0.5f,
+			-0.5f, -0.5f, 0.0f, 0.0f, // 0
+			 0.5f, -0.5f, 1.0f, 0.0f, // 1
+			 0.5f,  0.5f, 1.0f, 1.0f, // 2
+			-0.5f,  0.5f, 0.0f, 1.0f, // 3
 		};
 
 		unsigned int indices[] = {
@@ -53,19 +57,29 @@ int main(void) {
 			2, 3, 0
 		};
 
+		GLCall(glEnable(GL_BLEND));
+		GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+
 		VertexArray va;
-		VertexBuffer vb(positions, 4 * 2 * sizeof(float));
+		VertexBuffer vb(positions, 4 * 4 * sizeof(float));
 
 		VertexBufferLayout layout;
+		layout.Push<float>(2);
 		layout.Push<float>(2);
 		va.AddBuffer(vb, layout);
 
 		IndexBuffer ib(indices, 6);
 
+		mat4 proj = ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f);
+
 		Shader shader("res/shaders/Basic.shader");
 		shader.Bind();
-
 		shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
+		shader.SetUniformMat4f("u_MVP", proj);
+
+		Texture texture("res/textures/ChernoLogo.png");
+		texture.Bind();
+		shader.SetUniform1i("u_Texture", 0);
 
 		va.Unbind();
 		vb.Unbind();
--- OpenGL/src/Shader.h
+++ OpenGL/src/Shader.h
@@ -2,6 +2,8 @@
 #include <string>
 #include <unordered_map>
 
+#include "glm/glm.hpp"
+
 struct ShaderProgramSource
 {
 	std::string VertexSource;
@@ -22,7 +24,11 @@ public:
 	void Bind() const;
 	void Unbind() const;
 
+
+	void SetUniform1i(const std::string& name, int v1);
+	void SetUniform1f(const std::string& name, float v1);
 	void SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
+	void SetUniformMat4f(const std::string& name, const glm::mat4 matrix);
 private:
 	ShaderProgramSource ParseShader(const std::string& filepath);
 	unsigned int CompileShader(unsigned int type, const std::string& source);
