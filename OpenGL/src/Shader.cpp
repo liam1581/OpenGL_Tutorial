afc1ff5d231407df765e789cbaa69911b7068539
liam1581 <liamschmallowsky123@gmail.com>
2026-03-29 17:34:04 +0200

    finished make uniforms faster

--- OpenGL/src/Shader.cpp
+++ OpenGL/src/Shader.cpp
@@ -108,7 +108,7 @@ void Shader::SetUniformMat4f(const std::string& name, const glm::mat4 matrix) {
 	GLCall(glUniformMatrix4fv(GetUniformLocation(name), 1, GL_FALSE, &matrix[0][0]));
 }
 
-int Shader::GetUniformLocation(const string& name) {
+int Shader::GetUniformLocation(const string& name) const {
 	if (m_UniformLocationCache.find(name) != m_UniformLocationCache.end())
 		return m_UniformLocationCache[name];
 	GLCall(int location = glGetUniformLocation(m_RendererID, name.c_str()));
