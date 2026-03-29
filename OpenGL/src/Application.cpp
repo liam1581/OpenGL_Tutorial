d4962a5925718cffca512c9ac07886004c2c5aeb
liam1581 <liamschmallowsky123@gmail.com>
2026-03-29 16:37:42 +0200

    finished setting up test framework

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -9,16 +9,17 @@
 #include "IndexBuffer.h"
 #include "Renderer.h"
 #include "Shader.h"
+#include "Texture.h"
 #include "VertexArray.h"
 #include "VertexBuffer.h"
 #include "VertexBufferLayout.h"
-#include "Texture.h"
+#include "tests/TestClearColor.h"
 
-#include "glm/glm.hpp"
-#include "glm/gtc/matrix_transform.hpp"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
-#include "imgui/imgui.h"
-#include "imgui/imgui_impl_glfw_gl3.h"
+#include <imgui/imgui.h>
+#include <imgui/imgui_impl_glfw_gl3.h>
 
 using namespace std;
 
@@ -50,46 +51,8 @@ int main(void) {
 
 	cout << glGetString(GL_VERSION) << endl;
 	{
-		float positions[] = {
-			 100.0f,  100.0f, 0.0f, 0.0f, // 0
-			 200.0f,  100.0f, 1.0f, 0.0f, // 1
-			 200.0f,  200.5f, 1.0f, 1.0f, // 2
-			 100.0f,  200.5f, 0.0f, 1.0f, // 3
-		};
-
-		unsigned int indices[] = {
-			0, 1, 2,
-			2, 3, 0
-		};
-
 		GLCall(glEnable(GL_BLEND));
 		GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
-
-		VertexArray va;
-		VertexBuffer vb(positions, 4 * 4 * sizeof(float));
-
-		VertexBufferLayout layout;
-		layout.Push<float>(2);
-		layout.Push<float>(2);
-		va.AddBuffer(vb, layout);
-
-		IndexBuffer ib(indices, 6);
-
-		glm::mat4 proj = glm::ortho(0.0f, (float)width, 0.0f, (float)height, -1.0f, 1.0f);
-		glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
-
-		Shader shader("res/shaders/Basic.shader");
-		shader.Bind();
-		shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
-
-		Texture texture("res/textures/ChernoLogo.png");
-		texture.Bind();
-		shader.SetUniform1i("u_Texture", 0);
-
-		va.Unbind();
-		vb.Unbind();
-		ib.Unbind();
-		shader.Unbind();
 
 		Renderer renderer;
 
@@ -97,37 +60,16 @@ int main(void) {
 		ImGui_ImplGlfwGL3_Init(window, true);
 		ImGui::StyleColorsDark();
 
-		glm::vec3 translation(200, 200, 0);
-
-		float r = 0.0f;
-		float increment = 0.05f;
+		test::TestClearColor test;
 
 		while (!glfwWindowShouldClose(window)) {
 			renderer.Clear();
 
+			test.OnUpdate(0.0f);
+			test.OnRender();
+
 			ImGui_ImplGlfwGL3_NewFrame();
-
-			glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
-			glm::mat4 mvp = proj * view * model;
-
-			shader.Bind();
-			shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
-			shader.SetUniformMat4f("u_MVP", mvp);
-
-			renderer.Draw(va, ib, shader);
-
-			if (r > 1.0f)
-				increment = -0.05f;
-			else if (r < 0.0f)
-				increment = 0.05f;
-
-			r += increment;
-
-			{
-				ImGui::SliderFloat3("Translation", &translation.x, 0.0f, (float)width);
-				ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-			}
-
+			test.OnImGuiRender();
 			ImGui::Render();
 			ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
 
--- OpenGL/src/Shader.h
+++ OpenGL/src/Shader.h
@@ -2,7 +2,7 @@
 #include <string>
 #include <unordered_map>
 
-#include "glm/glm.hpp"
+#include <glm/glm.hpp>
 
 struct ShaderProgramSource
 {
